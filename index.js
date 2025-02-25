79ecf970c2c2befeb3a79c867bc5c5e8d83e056a
Sam Willard <144750636+willarsa@users.noreply.github.com>
2025-02-24 20:54:20 -0800

    got it basically working
    if wanting to make a bigger thing then I will basically have to rewrite everything to get it into standard website form

---
+++ index.js
@@ -0,0 +1,83 @@
+let playerHealth = [20]
+let playerCount = 1
+let health = 20
+let num = 0;
+
+
+function addToDisplay(element, input){
+    let container = element.closest('[data-id]')
+    let dataId = container.getAttribute('data-id')
+    let sideDisplay = container.querySelector(`#side-display`)
+    
+    if(sideDisplay.value === "Error"){
+        sideDisplay.value = ""
+    }
+    sideDisplay.value += input
+}
+
+function calculate(element, input){
+    let container = element.closest('[data-id]')
+    let dataId = container.getAttribute('data-id')
+    let sideDisplay = container.querySelector(`#side-display`)
+    let display = container.querySelector(`#display`)
+
+    if(sideDisplay.value === "Error"){
+        sideDisplay.value = ""
+    }
+    if(num === 1){
+        sideDisplay.value += '+'
+    } else {
+        sideDisplay.value += '-'
+    }
+
+    let length = sideDisplay.value.length
+    if((sideDisplay.value.charAt(length-1) === "-" || sideDisplay.value.charAt(length-1) === "+") && sideDisplay.value.length > 1){
+        for(let i = length-2; i >= 0; i--){
+            if(sideDisplay.value.charAt(i) === "-" || sideDisplay.value.charAt(i) === "+"){
+                sideDisplay.value = "Error"
+                return
+            }
+        }
+
+        num = parseInt(sideDisplay.value.slice(0, length-1))
+        
+        health += num * (input)
+        num = 0
+
+        display.value = parseFloat(health)
+        sideDisplay.value = ""
+    }
+    else{
+        sideDisplay.value = "Error"
+    }
+}
+
+function setHealth(element, input){
+    let container = element.closest('[data-id]')
+    let dataId = container.getAttribute('data-id')
+    let display = container.querySelector(`#display`)
+
+
+    health = input
+    console.log(parseFloat(health)) //shows as 20
+    display.value = parseFloat(health)
+}
+
+function clearSide(element){
+    let container = element.closest('[data-id]')
+    let dataId = container.getAttribute('data-id')
+    let sideDisplay = container.querySelector(`#side-display`)
+
+    sideDisplay.value = ""
+}
+
+function addPlayer(){
+    playerCount++
+    playerHealth.push(20)
+
+    const calculator = document.getElementById("calculator")
+    const duplicate = calculator.cloneNode(true)
+    duplicate.dataset.id = playerCount
+
+    document.body.appendChild(duplicate)
+}
